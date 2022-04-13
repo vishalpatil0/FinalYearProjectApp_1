ecd1edc3a050a0a09173cc6450444f080ca664f7
vishalpatil0 <vishalgpatil10@gmail.com>
2022-04-13 21:16:56 +0530

    First Commit

---
+++ app/src/main/java/com/example/splashscreen/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ app/src/main/java/com/example/splashscreen/SplashActivity.java
@@ -0,0 +1,54 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    Animation top,bottom;
+    ImageView logo;
+    TextView slogan,title;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        //Animations
+        top= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottom=AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+        logo=findViewById(R.id.logo);
+        title=findViewById(R.id.title);
+        slogan=findViewById(R.id.slogan);
+        logo.setAnimation(top);
+        title.setAnimation(bottom);
+        slogan.setAnimation(bottom);
+
+        //hides the action bar
+        getSupportActionBar().hide();
+
+        Thread thread=new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    sleep(3000);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent i=new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        thread.start();
+    }
+}
