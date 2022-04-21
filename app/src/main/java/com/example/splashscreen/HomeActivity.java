c5045da24db52b12ceec71705885c21cdee55f17
vishalpatil0 <vishalgpatil10@gmail.com>
2022-04-21 13:43:27 +0530

    Minor changes in activity_home.xml linearlayout to textview with image

--- app/src/main/java/com/example/splashscreen/HomeActivity.java
+++ app/src/main/java/com/example/splashscreen/HomeActivity.java
@@ -1,14 +1,26 @@
 package com.example.splashscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
+    CardView attestation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        attestation=(CardView) findViewById(R.id.attestation);
+        attestation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(HomeActivity.this,AttestationActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
