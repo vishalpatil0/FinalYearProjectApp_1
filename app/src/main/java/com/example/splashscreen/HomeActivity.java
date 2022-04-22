01bb75d858158d3e510a58cd7589bce4dd25686c
vishalpatil0 <vishalgpatil10@gmail.com>
2022-04-22 16:44:20 +0530

    Added register activity

--- app/src/main/java/com/example/splashscreen/HomeActivity.java
+++ app/src/main/java/com/example/splashscreen/HomeActivity.java
@@ -9,10 +9,18 @@ import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private CardView Attestation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
+        Attestation=(CardView) findViewById(R.id.attestation);
+        Attestation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(HomeActivity.this,AttestationActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/splashscreen/LoginActivity.java
+++ app/src/main/java/com/example/splashscreen/LoginActivity.java
@@ -4,20 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-    private Button login;
+    private Button login,signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         login=findViewById(R.id.login);
+        signup=findViewById(R.id.signup);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i=new Intent(LoginActivity.this,HomeActivity.class);
+                startActivity(i);
+            }
+        });
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(LoginActivity.this,RegisterActivity.class);
                 startActivity(i);
             }
         });
