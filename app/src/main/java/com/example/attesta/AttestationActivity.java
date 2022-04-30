948cc93ca35bc562ff127a5d72f12cfaf8271a3a
vishalpatil0 <vishalgpatil10@gmail.com>
2022-04-30 14:41:21 +0530

    Added validation java class

--- app/src/main/java/com/example/attesta/AttestationActivity.java
+++ app/src/main/java/com/example/attesta/AttestationActivity.java
@@ -1,10 +1,16 @@
 package com.example.attesta;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,50 +18,91 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.github.dhaval2404.imagepicker.ImagePicker;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class AttestationActivity extends AppCompatActivity {
+public class AttestationActivity extends AppCompatActivity{
 
     private FloatingActionButton originalImageButton,formImageButton;
-    private ImageView orignalImage,formImage;
+    private ImageView originalImage,formImage;
+    private Button validate,ready;
+    private String originalImageString="vishal",formImageString;
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attestation);
+
+        //Initializing objects with their respective resource IDs
         originalImageButton=(FloatingActionButton) findViewById(R.id.originalImageButton);
-        orignalImage=(ImageView)findViewById(R.id.originalImage);
+        originalImage =(ImageView)findViewById(R.id.originalImage);
         formImageButton=(FloatingActionButton)findViewById(R.id.formImageButton);
         formImage=(ImageView)findViewById(R.id.formImage);
+        validate=(Button)findViewById(R.id.validate);
+        progressBar=findViewById(R.id.progressBar);
+
+        /**
+         * originalImageButton on click listener to launch the Imagepicker library
+         */
         originalImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 ImagePicker.with(AttestationActivity.this).crop().start(101);
             }
         });
+
+        /**
+         * formImageButton to import the image.
+         */
         formImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 ImagePicker.with(AttestationActivity.this).crop().start(102);
             }
         });
+
+        /**
+         * Validate function here will compare the UID from 2 images.
+         */
+        validate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+                if(originalImage.getDrawable()!=null)
+                {
+                    BitmapDrawable bitmapDrawable= (BitmapDrawable) originalImage.getDrawable();
+                    Bitmap originalImageBitmap = bitmapDrawable.getBitmap();
+                    String UID="";
+                    Validation validation=new Validation(AttestationActivity.this,originalImageBitmap);
+                    UID=validation.detect();
+                    Toast.makeText(AttestationActivity.this, "Asim : "+UID, Toast.LENGTH_SHORT).show();
+
+                }
+                else
+                {
+                    Toast.makeText(AttestationActivity.this, "Please import your images first", Toast.LENGTH_SHORT).show();
+                }
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 
+    /**
+     *
+     * @param requestCode to identify which view have requested the data
+     * @param resultCode  not needed here
+     * @param data  contains the image
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode==101)
         {
             Uri uri=data.getData();
-            orignalImage.setImageURI(uri);
-            //Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),uri);
-            //originalImage.setImageBitmap(bitmap);
+            originalImage.setImageURI(uri);
         }
         else if(requestCode==102)
         {
             Uri uri=data.getData();
             formImage.setImageURI(uri);
-            //formImage.invalidate();
-            //BitmapDrawable bitmapDrawable=(BitmapDrawable) formImage.getDrawable();
-            //Bitmap bitmap=bitmapDrawable.getBitmap();
-            //originalImage.setImageBitmap(bitmap);
         }
     }
+
 }
---
+++ app/src/main/java/com/example/attesta/Validation.java
@@ -0,0 +1,94 @@
+package com.example.attesta;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
+
+import java.util.List;
+
+public class Validation {
+    private int startIndex=-1,endIndex=0,count=0;
+    private InputImage originalImage;
+    private Context context;
+    private String UID_Data="";
+    private TextRecognizer recognizer;
+
+    /**
+     *
+     * @param bitmap bitmap of images from imageviews on attesation_activity
+     */
+    Validation(Context context,Bitmap bitmap)
+    {
+        this.context=context;
+        originalImage=InputImage.fromBitmap(bitmap,0);
+        recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+    }
+
+    /**
+     *
+     * @return string which contain the recognized text
+     */
+    public String detect()
+    {
+        final String[] UID = {""};
+        Task<Text> result =
+                recognizer.process(originalImage)
+                        .addOnSuccessListener(new OnSuccessListener<Text>() {
+                            @Override
+                            public void onSuccess(Text visionText) {
+                                Log.d("recognizer is successful","InputImage got recognized successfully");
+                            }
+                        })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(context, "Failed to detect text form image..", Toast.LENGTH_SHORT).show();
+                                    }
+
+                                });
+
+        //Infinite loop
+        while(true)
+        {
+            if(result.isComplete())
+            {
+                if(result.isSuccessful())
+                {
+                    List<Text.TextBlock> textBlocks=result.getResult().getTextBlocks();
+                    for(int i=0;i<textBlocks.size();i++)
+                    {
+                        List<Text.Line> textLines=textBlocks.get(i).getLines();
+                        for(int j=0;j<textLines.size();j++)
+                        {
+                            List<Text.Element> elements=textLines.get(j).getElements();
+                            for(int k=0;k<elements.size();k++)
+                            {
+                                UID[0] +=elements.get(k).getText();
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    UID[0]="Failed";
+                    Log.d("result failed","TextRecognizer failed due to some reason");
+                }
+                break;
+            }
+        }
+        return UID[0];
+    }
+}
